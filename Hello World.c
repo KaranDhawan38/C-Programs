6dacbf8a0ab516f54bfc5debcabdc6aa7d1563db
KaranDhawan38 <42664456+KaranDhawan38@users.noreply.github.com>
2018-10-07 15:34:07 +0530

    First program

---
+++ Hello World.c
@@ -0,0 +1,17 @@
+/*
+ C Programming
+
+ Write a code to print "Hello World".
+
+ created 7 Oct 2018
+ by Karan Dhawan
+
+ This example code is in the public domain.
+*/
+
+#include<stdio.h>
+int main()
+{
+  printf("\"Hello World\""); //We use \ to print ",\.
+  return 0;
+}
