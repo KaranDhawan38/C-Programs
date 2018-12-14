384bdd16cda276452b375bb45a3332917997f65b
unknown <karandhawan2014@gmail.com>
2018-12-14 11:47:03 +0530

    Adding string to code

---
+++ stringInputOutput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    //////Normal string printing//
+    char str[20] = "Karan Dhawan";
+    printf ("%s\n",str);
+    ///////to take input//////////
+    gets(str);
+    printf ("%s",str);
+    return 0;
+}
